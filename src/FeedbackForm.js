31e2ab69708cd44b72b9373e6fc258e335ce9b81
Rabia Ashraf <19-20911-036@cs.fjwu.edu.pk>
2023-09-04 11:36:47 +0500

    commit

--- src/FeedbackForm.js
+++ src/FeedbackForm.js
@@ -1,22 +1,22 @@
 import React from 'react';
 
-const FeedbackForm = () => {
+const FeedbackForm = ({ recipient, subject }) => {
   const handleFeedback = () => {
-    const recipient = 'rabiaashrafpk233@gmail.com'; 
-    const subject = 'Feedback for Pedagogic Quran E-Book'; 
-
     const mailtoLink = `mailto:${recipient}?subject=${encodeURIComponent(subject)}`;
-
     window.location.href = mailtoLink;
   };
 
   return (
     <div className="feedback-container">
-      <h2 className ="feedback-title">We value your feedback!</h2>
-      <p className= "Paragraph">
-         If you have any suggestions, questions, or comments</p>
-       <p className= "Paragraph"> regarding our Pedagogic Quran E-Book, please let us know by filling out</p> 
-      <p className= "Paragraph">  the form below. Your feedback will help us improve our service. Thank
+      <h2 className="feedback-title">We value your feedback!</h2>
+      <p className="Paragraph">
+        If you have any suggestions, questions, or comments
+      </p>
+      <p className="Paragraph">
+        regarding our Pedagogic Quran E-Book, please let us know by filling out
+      </p>
+      <p className="Paragraph">
+        the form below. Your feedback will help us improve our service. Thank
         you so much!
       </p>
       <button className="feedback-button" onClick={handleFeedback}>
